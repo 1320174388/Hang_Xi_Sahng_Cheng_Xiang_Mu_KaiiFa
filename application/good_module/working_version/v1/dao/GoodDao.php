0882c3d3aea10d91e95ef0297e6e49fc1cf52d44
时广玉 <shiguangyu@dlaotianhuang.com>
2018-08-04 17:32:44 +0800

    2018/08/04 16:41 获取商品信息接口开发中

--- application/good_module/working_version/v1/dao/GoodDao.php
+++ application/good_module/working_version/v1/dao/GoodDao.php
@@ -478,7 +478,7 @@ class GoodDao implements GoodInterface
         }
 
         // 判断分类是不是顶级分类
-        if($classData['class_parent']!=0) {
+        if($classData['class_parent']!==0) {
             // 处理查询条件
             $goodModel = GoodModel::where(
                 'class_index',
@@ -487,9 +487,9 @@ class GoodDao implements GoodInterface
         }
 
         // 获取商品数据
-        $goodList = $goodModel->find();
+        $goodList = $goodModel->select()->toArray();
 
         // 返回正确数据
-        return returnData('success',$get['classIndex']);
+        return returnData('success',$goodList);
     }
 }
